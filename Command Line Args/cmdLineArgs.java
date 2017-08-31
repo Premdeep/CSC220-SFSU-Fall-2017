d329f81afd7628a7394a3efd32b62216733d0449
Premdeep <preamdhep.usapp@gmail.com>
2017-08-31 13:09:44 -0700

    Command Line Arguments Example

---
+++ Command Line Args/cmdLineArgs.java
@@ -0,0 +1,12 @@
+public class cmdLineArgs{
+
+	public static void main(String[] args){
+		// Example on using Cmd Line Args
+		System.out.println("cmd line args are : ");
+		for(String s : args)
+			System.out.println(s);
+
+
+	}
+}
+		

82e9b7dfb72821f261abb03d142d8d1928c556f2
Premdeep <preamdhep.usapp@gmail.com>
2017-09-08 12:55:16 -0700

    delteIndex method implementation

--- linkedList/linkedList.java
+++ linkedList/linkedList.java
@@ -8,84 +8,128 @@ public class linkedList{
 	 		size = 0;
 	 }
 
- 	public void appendNode(int data){
+	 public void appendNode(int data){
 
-	 	node n = new node(data);
-	 	if(head == null){
-	 		head = n;
-	 		++size;
+	 	node newNode = new node(data);
+	 	if(head == null)
+	 		head = newNode;
+	 	else{
+	 		node last = head;
+	 		while(last.next != null)
+	 			last = last.next;
+	 		last.next = newNode;
+	 	}
+	 	
+	 	++size;
+	 	
+	 }
+
+	 public void insertFront(int data){
+	 	node newNode = new node(data);
+	 	newNode.next = head;
+	 	head = newNode;
+	 	++size;
+	 }
+
+	 public void printList(){
+	 	if(head == null)
 	 		return;
-		 }
-		 else{
-		 	node last = head;
-		 	while(last.next != null)
-		 		last = last.next;
-		 	last.next = n;
-		 	++size;
+	 	node walk = head;
+	 	while(walk != null){
+	 		System.out.print(walk.data+" -> ");
+	 		walk = walk.next;
+	 	}
+	 	System.out.println("null");
+	 }
 
-		 }
+	 public void size(){
+	 	System.out.println("Size is : "+size);
+	 }
+
+	 public void removelast(){
+	 	if(isEmpty())
+	 		return;
+	 	if(head.next == null){
+	 		head = null;
+	 		return;
 	 	}
 
-	 	public boolean isEmpty(){
-	 		return (size == 0);
+	 	node last = head;
+	 	node prev = head;
+	 	while(last.next != null){
+	 		prev = last;
+	 		last = last.next;	 	
+		 }
+		 prev.next = null;
+		 --size;
+
+	 }
+
+	 public boolean isEmpty(){
+	 	return (size == 0);
+	 }
+
+	 public void removeFront(){
+	 	if(isEmpty())
+	 		return;
+	 	head = head.next;
+	 	--size;
+	 }
+
+	 public void deleteIndex(int i ){
+
+	 	if(isEmpty())
+	 		return;
+
+	 	if(i >= size){
+	 		System.out.println("Index out of bound error");
+	 		return;
 	 	}
 
+	 	if(i == 0){
+	 		head = head.next;
+	 		--size;
+	 		return;
+	 	}
 
-	public void printList(){
-		// if(head == null)
-		// 	return;
-		if(isEmpty())
-			return;
+	 	node prev = null;
+	 	node curr = head;
 
-		node walk = head;
+	 	while(i != 0){
+	 		prev = curr;
+	 		curr = curr.next;
+	 		i--;
+	 	}
 
-		while(walk != null){
-			System.out.print(walk.data+" -> ");
-			walk = walk.next;
-		}
-		System.out.println("null");
-
-	}
-
-	public void listSize(){
-		System.out.println("Size is : "+size);
-	}
-
-	public void insertFront(int data){
-		node first = new node(data);
-		first.next = head;
-		head = first;
-		++size;
-	}
-
-	public void removeFront(){
-		if(isEmpty())
-			return;
-		head = head.next;
-		--size;
-	}
+	 	prev.next = curr.next;
+	 	--size;
+	 }
 
 
 
 	public static void main(String[] args){
 
 		linkedList l = new linkedList();
-		l.listSize();
-
-		l.appendNode(2);
+		//l.size();
 		l.appendNode(1);
-		l.appendNode(5);	
-
+		l.appendNode(2);
+		l.appendNode(3);
+		//l.printList();
+		//l.size();
 		l.insertFront(4);
-		l.insertFront(9);
+		l.insertFront(5);
+		//l.printList();
+		//l.size();
+		//l.removelast();
+		//l.removelast();
+		//l.printList();
+		//l.size();
+		//l.removeFront();
 		l.printList();
-		l.listSize();
-
-		l.removeFront();
-		l.removeFront();
-
+		l.size();
+		l.deleteIndex(1);
 		l.printList();
-		l.listSize();
+		l.size();
 
 	}
 }

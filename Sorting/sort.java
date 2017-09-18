fafedca3a93b61d0c0c34262681138595265964c
Premdeep <preamdhep.usapp@gmail.com>
2017-09-18 16:40:13 -0700

    added insertion and selection sorts

--- Sorting/sort.java
+++ Sorting/sort.java
@@ -22,12 +22,45 @@ public class sort{{}
 		}
 	}
 
+	public static void iSort(int[] a, int size){
+
+		for(int unsorted = 1; unsorted < size; unsorted++){
+			for(int sorted  = 0; sorted < unsorted; sorted++){
+
+				if(a[sorted] > a[unsorted])
+					swap(a, sorted, unsorted);
+			}
+		}
+	}
+
+	public bSort(int[] a , int size){
+		boolean swapped = true;
+		while(swapped){
+			swapped = false;
+			for(int i = 0; i < size; i++){
+				if(a[i] > a[i+1]){
+					swap(a, i , i+1);
+					swapped = true;
+				}
+			}
+		}
+	}
+	
+
+	
+	
+	
+
 	public static void main(String[] args){
 
 		int[] a  = {5,4,1,3,2};
+		int[] b = {1,2,3,4,5,6,7,8,9};
+
 		print(a,5);
-		sSort(a, 5);
+		iSort(a, 5);
 		print(a,5);
+
+		
 
 	}
 }
